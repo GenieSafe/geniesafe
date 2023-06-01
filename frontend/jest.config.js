8d47b4045f3fdbf4f253c18d829d07e9ce1001b8
alifmazli <alifmazli01@gmail.com>
2023-06-02 03:09:42 +0800

    Add Jest and Cypress

---
+++ frontend/jest.config.js
@@ -0,0 +1,10 @@
+import * as nextJest from 'next/jest'
+
+const createJestConfig = nextJest({ dir: './' })
+
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
