21669e4d8daccc06066e3ff40e4948e6f4b2c6c6
alifmazli <alifmazli01@gmail.com>
2023-06-01 22:46:41 +0800

    Comment out non-essential code

--- frontend/components/LocalFaucetButton.tsx
+++ frontend/components/LocalFaucetButton.tsx
@@ -1,52 +1,52 @@
-import { Button, useToast } from '@chakra-ui/react'
-import { ethers, providers } from 'ethers'
-import { useCallback } from 'react'
-import { useAccount } from 'wagmi'
-import { useCheckLocalChain } from '../src/hooks/useCheckLocalChain'
+// import { Button, useToast } from '@chakra-ui/react'
+// import { ethers, providers } from 'ethers'
+// import { useCallback } from 'react'
+// import { useAccount } from 'wagmi'
+// import { useCheckLocalChain } from '../src/hooks/useCheckLocalChain'
 
-/**
- * Constants & Helpers
- */
+// /**
+//  * Constants & Helpers
+//  */
 
-const localProvider = new providers.StaticJsonRpcProvider(
-  'http://localhost:8545'
-)
+// const localProvider = new providers.StaticJsonRpcProvider(
+//   'http://localhost:8545'
+// )
 
-/**
- * Component
- */
-export const LocalFaucetButton = () => {
-  const { address } = useAccount()
+// /**
+//  * Component
+//  */
+// export const LocalFaucetButton = () => {
+//   const { address } = useAccount()
 
-  const { isLocalChain } = useCheckLocalChain()
+//   const { isLocalChain } = useCheckLocalChain()
 
-  const toast = useToast()
+//   const toast = useToast()
 
-  // Use the localProvider as the signer to send ETH to our wallet
-  const sendFunds = useCallback(async () => {
-    if (address) {
-      const signer = localProvider.getSigner()
+//   // Use the localProvider as the signer to send ETH to our wallet
+//   const sendFunds = useCallback(async () => {
+//     if (address) {
+//       const signer = localProvider.getSigner()
 
-      const transaction = await signer.sendTransaction({
-        to: address,
-        value: ethers.constants.WeiPerEther,
-      })
+//       const transaction = await signer.sendTransaction({
+//         to: address,
+//         value: ethers.constants.WeiPerEther,
+//       })
 
-      await transaction.wait()
+//       await transaction.wait()
 
-      toast({
-        title: 'Transaction Successful',
-        description: 'ETH sent from local faucet',
-        status: 'success',
-        duration: 5000,
-        isClosable: true,
-      })
-    }
-  }, [address, toast])
+//       toast({
+//         title: 'Transaction Successful',
+//         description: 'ETH sent from local faucet',
+//         status: 'success',
+//         duration: 5000,
+//         isClosable: true,
+//       })
+//     }
+//   }, [address, toast])
 
-  return (
-    <Button colorScheme="teal" onClick={sendFunds} isDisabled={!isLocalChain}>
-      Send Funds From Local Hardhat Chain
-    </Button>
-  )
-}
+//   return (
+//     <Button colorScheme="teal" onClick={sendFunds} isDisabled={!isLocalChain}>
+//       Send Funds From Local Hardhat Chain
+//     </Button>
+//   )
+// }
--- frontend/components/NftList.tsx
+++ frontend/components/NftList.tsx
@@ -1,114 +1,114 @@
-import {
-  Alert,
-  AlertIcon,
-  Box,
-  Flex,
-  Image,
-  SimpleGrid,
-  Text,
-} from '@chakra-ui/react'
-import { Result } from '@ethersproject/abi'
-import { IPFSHTTPClient } from 'ipfs-http-client'
-import { useEffect, useState } from 'react'
+// import {
+//   Alert,
+//   AlertIcon,
+//   Box,
+//   Flex,
+//   Image,
+//   SimpleGrid,
+//   Text,
+// } from '@chakra-ui/react'
+// import { Result } from '@ethersproject/abi'
+// import { IPFSHTTPClient } from 'ipfs-http-client'
+// import { useEffect, useState } from 'react'
 
-interface NftListProps {
-  address?: string | null
-  ipfs: IPFSHTTPClient
-  nftTokenUris: Array<Result> | Array<unknown>
-}
+// interface NftListProps {
+//   address?: string | null
+//   ipfs: IPFSHTTPClient
+//   nftTokenUris: Array<Result> | Array<unknown>
+// }
 
-type NftMetadataType = {
-  description: string
-  image: string
-  name: string
-}
+// type NftMetadataType = {
+//   description: string
+//   image: string
+//   name: string
+// }
 
-export const NftList = ({
-  address,
-  ipfs,
-  nftTokenUris,
-}: NftListProps): JSX.Element => {
-  const [nfts, setNfts] = useState<Array<NftMetadataType>>([])
+// export const NftList = ({
+//   address,
+//   ipfs,
+//   nftTokenUris,
+// }: NftListProps): JSX.Element => {
+//   const [nfts, setNfts] = useState<Array<NftMetadataType>>([])
 
-  useEffect(() => {
-    const fetchNftData = async (ipfsHash: string) => {
-      try {
-        const resp = await ipfs.cat(ipfsHash)
-        let content: Array<number> = []
+//   useEffect(() => {
+//     const fetchNftData = async (ipfsHash: string) => {
+//       try {
+//         const resp = await ipfs.cat(ipfsHash)
+//         let content: Array<number> = []
 
-        for await (const chunk of resp) {
-          content = [...content, ...chunk]
-        }
+//         for await (const chunk of resp) {
+//           content = [...content, ...chunk]
+//         }
 
-        const raw = Buffer.from(content).toString('utf8')
+//         const raw = Buffer.from(content).toString('utf8')
 
-        return JSON.parse(raw)
-      } catch (error) {
-        console.log('error', error)
-      }
-    }
+//         return JSON.parse(raw)
+//       } catch (error) {
+//         console.log('error', error)
+//       }
+//     }
 
-    const processTokenUris = async () => {
-      const nftData = await Promise.all(
-        nftTokenUris.map(async (tokenUri: any = '') => {
-          if (tokenUri) {
-            const ipfsHash = tokenUri.replace('https://ipfs.io/ipfs/', '')
-            const ipfsData = await fetchNftData(ipfsHash)
-            return ipfsData
-          }
+//     const processTokenUris = async () => {
+//       const nftData = await Promise.all(
+//         nftTokenUris.map(async (tokenUri: any = '') => {
+//           if (tokenUri) {
+//             const ipfsHash = tokenUri.replace('https://ipfs.io/ipfs/', '')
+//             const ipfsData = await fetchNftData(ipfsHash)
+//             return ipfsData
+//           }
 
-          return {
-            image: '',
-            name: '',
-          }
-        })
-      )
+//           return {
+//             image: '',
+//             name: '',
+//           }
+//         })
+//       )
 
-      setNfts(nftData)
-    }
+//       setNfts(nftData)
+//     }
 
-    processTokenUris()
-  }, [ipfs, nftTokenUris])
+//     processTokenUris()
+//   }, [ipfs, nftTokenUris])
 
-  if (nftTokenUris.length === 0) {
-    return (
-      <Alert status="warning">
-        <AlertIcon />
-        No NFTs associated with your address: {address}
-      </Alert>
-    )
-  }
+//   if (nftTokenUris.length === 0) {
+//     return (
+//       <Alert status="warning">
+//         <AlertIcon />
+//         No NFTs associated with your address: {address}
+//       </Alert>
+//     )
+//   }
 
-  return (
-    <div>
-      <SimpleGrid my="6" columns={[1, 1, 2]} gap="6">
-        {nfts.map((nft) => {
-          return (
-            <Flex
-              key={nft.image}
-              p="4"
-              gap="4"
-              alignItems="center"
-              backgroundColor="white"
-              border="1px"
-              borderColor="gray.300"
-            >
-              <Image
-                boxSize={[100, 100, 200]}
-                objectFit="cover"
-                src={nft.image}
-                alt={nft.name}
-              />
-              <Box>
-                <Text fontSize="lg" fontWeight="semibold">
-                  {nft.name}
-                </Text>
-                <Text>{nft.description}</Text>
-              </Box>
-            </Flex>
-          )
-        })}
-      </SimpleGrid>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <SimpleGrid my="6" columns={[1, 1, 2]} gap="6">
+//         {nfts.map((nft) => {
+//           return (
+//             <Flex
+//               key={nft.image}
+//               p="4"
+//               gap="4"
+//               alignItems="center"
+//               backgroundColor="white"
+//               border="1px"
+//               borderColor="gray.300"
+//             >
+//               <Image
+//                 boxSize={[100, 100, 200]}
+//                 objectFit="cover"
+//                 src={nft.image}
+//                 alt={nft.name}
+//               />
+//               <Box>
+//                 <Text fontSize="lg" fontWeight="semibold">
+//                   {nft.name}
+//                 </Text>
+//                 <Text>{nft.description}</Text>
+//               </Box>
+//             </Flex>
+//           )
+//         })}
+//       </SimpleGrid>
+//     </div>
+//   )
+// }
