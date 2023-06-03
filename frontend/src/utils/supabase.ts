5bb284e6cde5a64e725fed2e7cb3a7bbd532b9da
alifmazli <alifmazli01@gmail.com>
2023-06-04 05:01:19 +0800

    Instantiate Supabase client

---
+++ frontend/src/utils/supabase.ts
@@ -0,0 +1,16 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Initialize Supabase client
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    db: {
+      schema: 'custom',
+    },
+    auth: {
+      persistSession: true,
+    },
+  }
+)
