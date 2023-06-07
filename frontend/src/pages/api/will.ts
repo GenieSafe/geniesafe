db016467f90205b67763702afac5ff8b5755d626
alifmazli <alifmazli01@gmail.com>
2023-06-07 17:38:29 +0800

    Add first iteration of GET and POST methods for wills

---
+++ frontend/src/pages/api/will.ts
@@ -0,0 +1,92 @@
+import createHttpError from 'http-errors'
+import * as Yup from 'yup'
+import { NextApiHandler } from 'next'
+
+import { apiHandler } from '../../utils/api'
+import prisma from '../../utils/prisma'
+import { validateRequest } from '../../utils/yup'
+
+/**
+ * Handler for retrieving a will.
+ * Supports retrieving a will by willId or ownerId.
+ */
+const getWill: NextApiHandler = async (req, res) => {
+  const { ownerId } = req.query
+  const { willId } = req.query
+
+  // If willId is provided, return will with that willId
+  if (willId) {
+    try {
+      const will = await prisma.will.findUnique({
+        where: {
+          id: parseInt(willId as string),
+        },
+      })
+      if (!will)
+        throw new createHttpError.NotFound(
+          `Will (ID: ${willId}) does not exist!`
+        )
+      res.status(200).json({ data: will })
+    } catch (err) {
+      console.log(err)
+      throw new createHttpError.NotFound(
+        `Error retrieving will with willId: ${willId}!`
+      )
+    }
+  }
+
+  // If ownerId is provided, return all wills with that ownerId
+  if (ownerId) {
+    try {
+      const wills = await prisma.will.findMany({
+        where: {
+          ownerId: ownerId.toString(),
+        },
+      })
+      if (!wills)
+        throw new createHttpError.NotFound(
+          `User (ID: ${ownerId}) does not exist!`
+        )
+      res.status(200).json({ data: wills })
+    } catch (err) {
+      console.log(err)
+      throw new createHttpError.NotFound(
+        `Error retrieving wills with ownerId: ${ownerId}!`
+      )
+    }
+  } else {
+    // Else if ownerId is not provided or doesn't exist, throw an error
+    throw new createHttpError.NotFound(
+      `Query parameter (ownerId or willId) not provided!`
+    )
+  }
+}
+
+const willSchema = Yup.object().shape({
+  ownerId: Yup.string().required('Owner ID is required!'),
+})
+
+const createWill: NextApiHandler = async (req, res) => {
+  // const data = validateRequest(req.body, willSchema)
+  const ownerId = req.body.ownerId
+  console.log(ownerId)
+
+  try {
+    const newWill = await prisma.will.create({
+      data: {
+        ownerId: ownerId,
+      },
+    })
+    res.status(200).json({ data: newWill })
+  } catch (err) {
+    console.log(err)
+    throw new createHttpError.NotFound(
+      `Error creating will with ownerId: ${ownerId}! Check if user exists.`
+    )
+  }
+}
+
+export default apiHandler({
+  GET: getWill,
+  POST: createWill,
+})
