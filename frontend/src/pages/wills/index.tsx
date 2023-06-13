288c8c24fe0b95a7b4b3577910406ef555a3952a
Syaamil Faiq <syaamil1136@gmail.com>
2023-06-13 22:16:16 +0800

    remove unused import

--- frontend/src/pages/wills/index.tsx
+++ frontend/src/pages/wills/index.tsx
@@ -1,10 +1,6 @@
-import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { NextPage } from 'next'
 import React from 'react'
 
-import { Layout, Heading } from 'lucide-react'
 import { Plus } from 'lucide-react'
 
 import { WillCard } from '../../components/WillCard'
@@ -13,25 +9,23 @@ import Link from 'next/link'
 import Will from '../api/will'
 
 const Wills: NextPage = () => {
-  const session = useSession()
-  const supabase = useSupabaseClient()
 
   return (
     <>
-          <div className="container flex items-center justify-between pt-12 pb-8">
-            <h1 className="text-4xl font-bold tracking-tight scroll-m-20 lg:text-5xl">
-              Your Wills
-            </h1>
-            <Button variant={'outline'} className="dark" asChild>
-              <Link href="/wills/create">
-              <Plus className="w-4 h-4 mr-2" />
-              Create new will
-              </Link>
-            </Button>
-          </div>
-          <div className="container flex flex-col">
-            <WillCard />
-          </div>
+      <div className="container flex items-center justify-between pt-12 pb-8">
+        <h1 className="text-4xl font-bold tracking-tight scroll-m-20 lg:text-5xl">
+          Your Wills
+        </h1>
+        <Button variant={'outline'} className="dark" asChild>
+          <Link href="/wills/create">
+            <Plus className="w-4 h-4 mr-2" />
+            Create new will
+          </Link>
+        </Button>
+      </div>
+      <div className="container flex flex-col">
+        <WillCard />
+      </div>
     </>
   )
 }
