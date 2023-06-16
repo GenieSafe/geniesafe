ec7da268ac67148a5d4f85da4a34286d29f6fd58
Syaamil Faiq <syaamil1136@gmail.com>
2023-06-16 21:50:01 +0800

    config index done

--- frontend/src/pages/safeguard/index.tsx
+++ frontend/src/pages/safeguard/index.tsx
@@ -8,66 +8,96 @@ import {
   CardTitle,
   CardContent,
 } from '../../components/ui/card'
+import { useState, useEffect } from 'react'
 
-const Safeguard: NextPage = () => {
-  const valid = true
+function getConfig(walletRecoveryConfigId: number) {
+  // TODO: Change param to userId instead of configId
+  return fetch(
+    `http://localhost:3000/api/entrust?walletRecoveryConfigId=${walletRecoveryConfigId}`,
+    {
+      method: 'GET',
+    }
+  ).then((res) => res.json())
+}
 
-  if (valid == false) {
-    return (
-      <>
-        <div className="container flex flex-col gap-8 pt-12 pb-8">
-          <h1 className="text-4xl font-bold tracking-tight scroll-m-20">
-            It seems like you don't have any wallet recovery method for now.
-          </h1>
-          <p className="leading-7">
-            Worry you might lose access to your private key? Assign trusted
-            Verifiers to help safeguard your private key.
-          </p>
-          <Button asChild className='self-start' size={'lg'}>
-            <Link href="/safeguard/assign">Assign Verifiers</Link>
-          </Button>
-        </div>
-      </>
-    )
-  } else {
-    return (
-      <>
-        <div className="container pt-12 pb-8">
-          <div className="flex flex-col gap-4 mb-4">
-            <h1 className="text-4xl font-bold tracking-tight scroll-m-20 lg:text-5xl">
-              Recover your wallet
-            </h1>
-            <p className="mb-4 leading-7">
-              Lost access to your wallet? Notify your Verifiers to verify your
-              identity and we’ll send you your private key.
-            </p>
-          </div>
-          <div className="grid gap-8">
-            <Card className="dark">
-              <CardHeader>
-                <CardTitle className="flex justify-between text-2xl">
-                  Verifiers
-                  <Button size={'sm'} className="">
-                    <Edit3 className="w-4 h-4" />
-                  </Button>
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="flex gap-4">
-                <Card className="bg-primary">
-                  <CardContent className="grid pt-6">
-                    <p className="text-secondary">Ali bin Abu</p>
+const Safeguard = () => {
+  const [config, setConfig] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await getConfig(0)
+        if (response.data) {
+          setConfig(response.data)
+        } else {
+          console.error('No data found in the response.')
+        }
+      } catch (error) {
+        console.error('Error fetching config data:', error)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  console.log(config)
+
+  return (
+    <>
+      <div className="container flex flex-col gap-8 pt-12 pb-8">
+        {config.length > 0 ? (
+          <>
+            <div className="container pt-12 pb-8">
+              <div className="flex flex-col gap-4 mb-4">
+                <h1 className="text-4xl font-bold tracking-tight scroll-m-20 lg:text-5xl">
+                  Recover your wallet
+                </h1>
+                <p className="mb-4 leading-7">
+                  Lost access to your wallet? Notify your Verifiers to verify
+                  your identity and we’ll send you your private key.
+                </p>
+              </div>
+              <div className="grid gap-8">
+                <Card className="dark">
+                  <CardHeader>
+                    <CardTitle className="flex justify-between text-2xl">
+                      Verifiers
+                      <Button size={'sm'} className="">
+                        <Edit3 className="w-4 h-4" />
+                      </Button>
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent className="flex gap-4">
+                    <Card className="bg-primary">
+                      <CardContent className="grid pt-6">
+                        <p className="text-secondary">Ali bin Abu</p>
+                      </CardContent>
+                    </Card>
                   </CardContent>
                 </Card>
-              </CardContent>
-            </Card>
-          </div>
-          <div className="grid justify-end py-8">
-            <Button size={'lg'}>Notify Verifiers</Button>
-          </div>
-        </div>
-      </>
-    )
-  }
+              </div>
+              <div className="grid justify-end py-8">
+                <Button size={'lg'}>Notify Verifiers</Button>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <h1 className="text-4xl font-bold tracking-tight scroll-m-20">
+              It seems like you don't have any wallet recovery method for now.
+            </h1>
+            <p className="leading-7">
+              Worry you might lose access to your private key? Assign trusted
+              Verifiers to help safeguard your private key.
+            </p>
+            <Button asChild className="self-start" size={'lg'}>
+              <Link href="/safeguard/assign">Assign Verifiers</Link>
+            </Button>
+          </>
+        )}
+      </div>
+    </>
+  )
 }
 
 export default Safeguard
