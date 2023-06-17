dfceb0f6424bfe1f8e4cf511bf69e4334abce487
Syaamil Faiq <syaamil1136@gmail.com>
2023-06-18 01:07:05 +0800

    will card clickable

--- frontend/src/components/WillCard.tsx
+++ frontend/src/components/WillCard.tsx
@@ -1,6 +1,5 @@
-import { CheckCircle2 } from 'lucide-react'
+import { CheckCircle2, Edit, Edit2 } from 'lucide-react'
 
-import { cn } from '../lib/utils'
 import {
   Card,
   CardContent,
@@ -10,69 +9,90 @@ import {
   CardTitle,
 } from '../components/ui/card'
 import { Badge } from './ui/badge'
+import Link from 'next/link'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from './ui/tooltip'
+import { Separator } from './ui/separator'
 
 type CardProps = React.ComponentProps<typeof Card>
 
 export function WillCard({ className, ...props }: CardProps) {
-  const { title, deployedAtBlock, isValidated } = props.data
+  const { title, deployedAtBlock, isValidated, id } = props.data
   return (
-    <Card className="dark">
-      <CardHeader className="flex-row justify-between columns-2">
-        <div>
-          <CardTitle className="text-2xl">{title}</CardTitle>
-          <CardDescription className="text-white">
-            Will contract deployed to
-            {deployedAtBlock !== null ? (
-              <span className="font-semibold">{deployedAtBlock}</span>
-            ) : (
-              <span className="font-semibold"> N/A</span>
-            )}
-          </CardDescription>
-        </div>
-        <div>
-          {isValidated ? (
-            <Badge className="bg-green-500">Validated</Badge>
-          ) : (
-            <Badge className="bg-red-500">Not Validated</Badge>
-          )}
-        </div>
-      </CardHeader>
-      <CardContent className="">
-        <div className="flex flex-col gap-4 pb-4">
-          <p className="font-bold">Beneficiaries</p>
-          <div className="flex gap-4">
-            {/* TODO: Retrieve list of beneficiaries before rendering these cards */}
-            <Card>
-              <CardContent className="pt-6">
-                <div className="flex flex-row gap-8">
-                  <div className="flex flex-col">
-                    <p className="text-lg font-semibold">Ali bin Abu</p>
-                    <p>0x1234567890</p>
-                    <p>Brother</p>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Link href={`wills/edit/${id}`}>
+            <Card className="dark">
+              <CardHeader className="grid grid-cols-2">
+                <div className="space-y-2">
+                  <CardTitle className="text-2xl">{title}</CardTitle>
+                  <CardDescription className="text-foreground">
+                    Will contract deployed to
+                    {deployedAtBlock !== null ? (
+                      <span className="font-semibold"> {deployedAtBlock}</span>
+                    ) : (
+                      <span className="font-semibold"> N/A</span>
+                    )}
+                  </CardDescription>
+                </div>
+                <div className="flex items-start justify-end">
+                  {isValidated ? (
+                    <Badge variant={'success'}>Active</Badge>
+                  ) : (
+                    <Badge className="" variant={'destructive'}>
+                      Unactive
+                    </Badge>
+                  )}
+                </div>
+              </CardHeader>
+              <CardContent className="grid gap-6">
+                <div className="grid gap-4">
+                  <p className="font-bold">Beneficiaries</p>
+                  <div className="flex gap-4 overflow-x-auto">
+                    {/* TODO: Retrieve list of beneficiaries before rendering these cards */}
+                    <Card>
+                      <CardContent className="pt-6">
+                        <div className="flex flex-row gap-8">
+                          <div className="flex flex-col">
+                            <p className="text-lg font-semibold">Ali bin Abu</p>
+                            <p>0x1234567890</p>
+                            <p>Brother</p>
+                          </div>
+                          <div className="flex flex-col items-center justify-center">
+                            <p className="text-2xl font-bold">50%</p>
+                          </div>
+                        </div>
+                      </CardContent>
+                    </Card>
                   </div>
-                  <div className="flex flex-col items-center justify-center">
-                    <p className="text-2xl font-bold">50%</p>
+                </div>
+                <div className="flex flex-col gap-4">
+                  <p className="font-bold">Validators</p>
+                  <div className="flex gap-4">
+                    {/* TODO: Retrieve list of validators before rendering these cards */}
+                    <Card className="">
+                      <CardContent className="pt-6">
+                        <div className="flex flex-row gap-8">
+                          <p>Ali bin Abu</p>
+                          <CheckCircle2 className="text-success"></CheckCircle2>
+                        </div>
+                      </CardContent>
+                    </Card>
                   </div>
                 </div>
               </CardContent>
             </Card>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4">
-          <p className="font-bold">Validators</p>
-          <div className="flex gap-4">
-            {/* TODO: Retrieve list of validators before rendering these cards */}
-            <Card className="">
-              <CardContent className="pt-6">
-                <div className="flex flex-row gap-8">
-                  <p>Ali bin Abu</p>
-                  <CheckCircle2 className="text-success"></CheckCircle2>
-                </div>
-              </CardContent>
-            </Card>
-          </div>
-        </div>
-      </CardContent>
-    </Card>
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Click to edit will</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
