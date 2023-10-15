f1a3f86e76059b8ad8f7e0c5e70a10103f3fde4b
Syaamil Faiq <syaamil1136@gmail.com>
2023-10-15 20:02:05 +0800

    Update interface attributes

--- frontend/types/interfaces.ts
+++ frontend/types/interfaces.ts
@@ -1,17 +1,31 @@
+export interface User {
+  id: string
+  email: string
+  walletAddress: string
+  firstName: string
+  lastName: string
+}
+
 export interface Will {
+  id: string
   ownerUserId: string
   title: string
-  walletAddress: string
-  beneficiaries: Beneficiary[]
-  validators: Validator[]
+  deployedAtBlock: string
+  isActive: boolean
+  isValidated: boolean
+  Beneficiaries: Beneficiary[]
+  Validators: Validator[]
 }
 
 export interface Beneficiary {
   beneficiaryUserId: string;
   percentage: number
+  User: User
 }
 export interface Validator {
   validatorUserId: string
+  isValidated: boolean
+  User: User
 }
 
 export interface Verifier {
