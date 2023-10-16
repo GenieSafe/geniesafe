09f8088fc17509f91429f3f8b3f1a046a90ce22b
Syaamil Faiq <syaamil1136@gmail.com>
2023-10-17 03:47:02 +0800

    Add protected-route api

---
+++ frontend/src/pages/api/protected-route.ts
@@ -0,0 +1,24 @@
+import { NextApiHandler } from 'next';
+import { createPagesServerClient } from '@supabase/auth-helpers-nextjs';
+import { Database } from '../../lib/database.types';
+
+const ProtectedRoute: NextApiHandler = async (req, res) => {
+	// Create authenticated Supabase Client
+	const supabase = createPagesServerClient<Database>({ req, res });
+	// Check if we have a session
+	const {
+		data: { session }
+	} = await supabase.auth.getSession();
+
+	if (!session)
+		return res.status(401).json({
+			error: 'not_authenticated',
+			description: 'The user does not have an active session or is not authenticated'
+		});
+
+	// Run queries with RLS on the server
+	const { data } = await supabase.from('profiles').select('*');
+	res.json(data);
+};
+
+export default ProtectedRoute;
