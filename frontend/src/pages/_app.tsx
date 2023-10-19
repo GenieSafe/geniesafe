13cd7b1c65e8ba7d8fb63dbdbb3a33efeddc1e5f
Syaamil Faiq <syaamil1136@gmail.com>
2023-10-19 18:33:45 +0800

    Remove comments

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,50 +1,10 @@
 
 import type { AppProps } from 'next/app'
-// import { configureChains, createClient, WagmiConfig } from 'wagmi'
-// import { mainnet, polygon, optimism, goerli, localhost } from 'wagmi/chains'
-// import { alchemyProvider } from 'wagmi/providers/alchemy'
-// import { publicProvider } from 'wagmi/providers/public'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { createPagesBrowserClient, Session } from '@supabase/auth-helpers-nextjs'
 import { useState } from 'react'
 import '../styles/global.css'
 import { Layout } from '../components/layout/Layout'
-
-// const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || ''
-
-// const { chains, provider, webSocketProvider } = configureChains(
-//   [
-//     mainnet,
-//     polygon,
-//     optimism,
-//     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
-//       ? [goerli, localhost]
-//       : []),
-//   ],
-//   [
-//     alchemyProvider({
-//       apiKey: ALCHEMY_API_KEY,
-//     }),
-//     publicProvider(),
-//   ]
-// )
-
-// const { wallets } = getDefaultWallets({
-//   appName: 'RainbowKit demo',
-//   chains,
-// })
-
-// const demoAppInfo = {
-//   appName: 'Rainbowkit Demo',
-// }
-
-// const connectors = connectorsForWallets(wallets)
-// const wagmiClient = createClient({
-//   autoConnect: true,
-//   connectors,
-//   provider,
-//   webSocketProvider,
-// })
 
 export default function App({ Component, pageProps }: AppProps<{
   initialSession: Session
@@ -57,20 +17,9 @@ export default function App({ Component, pageProps }: AppProps<{
       supabaseClient={supabaseClient}
       initialSession={pageProps.initialSession}
     >
-      {/* <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider
-          appInfo={demoAppInfo}
-          chains={chains}
-          theme={midnightTheme({
-            borderRadius: 'small',
-            fontStack: 'system',
-          })}
-        > */}
           <Layout>
             <Component {...pageProps} />
           </Layout>
-        {/* </RainbowKitProvider>
-      </WagmiConfig> */}
     </SessionContextProvider>
   )
 }
