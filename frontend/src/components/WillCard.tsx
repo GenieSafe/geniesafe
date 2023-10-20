f6db72e303c03fd1ecee98cd6c735167f2f0d71f
Syaamil Faiq <syaamil1136@gmail.com>
2023-10-21 04:24:28 +0800

    Add eth logo to will card

--- frontend/src/components/WillCard.tsx
+++ frontend/src/components/WillCard.tsx
@@ -20,13 +20,15 @@ import { Badge } from './ui/badge'
 
 import { Tables } from '../lib/database.types'
 
+import Image from 'next/image'
+
 export function WillCard({ will }: { will: any }) {
   return (
     <TooltipProvider>
       <Tooltip>
         <TooltipTrigger asChild>
           <Link href={`wills/edit/${will.id}`}>
-            <Card className="">
+            <Card className="hover:shadow-[0px_0px_20px_0px_hsl(var(--primary))] transition-shadow duration-500">
               <CardHeader className="grid grid-cols-2">
                 <div className="space-y-2">
                   <CardTitle className="text-2xl">{will.title}</CardTitle>
@@ -62,8 +64,11 @@ export function WillCard({ will }: { will: any }) {
                   <div className="flex gap-4 overflow-x-auto">
                     {will.beneficiaries ? (
                       will.beneficiaries.map(
-                        (beneficiary: Tables<'beneficiaries'>, index: number) => (
-                          <Card key={index} className='shadow-none'>
+                        (
+                          beneficiary: Tables<'beneficiaries'>,
+                          index: number
+                        ) => (
+                          <Card key={index} className="shadow-none">
                             <CardContent className="pt-6">
                               <div className="flex flex-row gap-8">
                                 <div className="flex flex-col w-24">
@@ -114,43 +119,67 @@ export function WillCard({ will }: { will: any }) {
 
                 <div className="flex flex-col gap-4">
                   <p className="font-bold">Validators</p>
-                  <div className="flex gap-4 overflow-x-auto">
-                    {will.validators ? (
-                      will.validators.map((validator: Tables<'validators'>, index: number) => (
-                        <Card key={index} className='shadow-none'>
-                          <CardContent className="pt-6">
-                            <div className="flex flex-row items-center gap-8">
-                              <div className="flex flex-col w-24">
-                                <p className="text-lg font-semibold truncate">
-                                  {
-                                    (validator.metadata as Record<string, any>)
-                                      .first_name
-                                  }{' '}
-                                  {
-                                    (validator.metadata as Record<string, any>)
-                                      .last_name
-                                  }
-                                </p>
-                                <p className="truncate">
-                                  {
-                                    (validator.metadata as Record<string, any>)
-                                      .wallet_address
-                                  }
-                                </p>
-                              </div>
+                  <div className="flex flex-row items-center justify-between">
+                    <div className="flex gap-4 overflow-x-auto">
+                      {will.validators ? (
+                        will.validators.map(
+                          (validator: Tables<'validators'>, index: number) => (
+                            <Card key={index} className="shadow-none">
+                              <CardContent className="pt-6">
+                                <div className="flex flex-row items-center gap-8">
+                                  <div className="flex flex-col w-24">
+                                    <p className="text-lg font-semibold truncate">
+                                      {
+                                        (
+                                          validator.metadata as Record<
+                                            string,
+                                            any
+                                          >
+                                        ).first_name
+                                      }{' '}
+                                      {
+                                        (
+                                          validator.metadata as Record<
+                                            string,
+                                            any
+                                          >
+                                        ).last_name
+                                      }
+                                    </p>
+                                    <p className="truncate">
+                                      {
+                                        (
+                                          validator.metadata as Record<
+                                            string,
+                                            any
+                                          >
+                                        ).wallet_address
+                                      }
+                                    </p>
+                                  </div>
 
-                              {validator.has_validated ? (
-                                <CheckCircle2 className="text-success"></CheckCircle2>
-                              ) : (
-                                <XCircle className="text-destructive"></XCircle>
-                              )}
-                            </div>
-                          </CardContent>
-                        </Card>
-                      ))
-                    ) : (
-                      <p className="">No validators found.</p>
-                    )}
+                                  {validator.has_validated ? (
+                                    <CheckCircle2 className="text-success"></CheckCircle2>
+                                  ) : (
+                                    <XCircle className="text-destructive"></XCircle>
+                                  )}
+                                </div>
+                              </CardContent>
+                            </Card>
+                          )
+                        )
+                      ) : (
+                        <p className="">No validators found.</p>
+                      )}
+                    </div>
+                    <div className='p-4'>
+                      <Image
+                        src="img/ethereum-eth-logo.svg"
+                        width={50}
+                        height={50}
+                        alt="ETH logo"
+                      />
+                    </div>
                   </div>
                 </div>
               </CardContent>
