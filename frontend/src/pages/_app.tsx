cf20716fe4b9215e1d9309f3f4068a89907a71e0
alif <alifmazli01@gmail.com>
2023-10-23 22:38:33 +0800

    Wrap _app with ThirdwebProvider

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,23 @@
-
 import type { AppProps } from 'next/app'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { createPagesBrowserClient, Session } from '@supabase/auth-helpers-nextjs'
+import {
+  createPagesBrowserClient,
+  Session,
+} from '@supabase/auth-helpers-nextjs'
+import {
+  ThirdwebProvider,
+  localWallet,
+  metamaskWallet,
+} from '@thirdweb-dev/react'
+import { Sepolia } from '@thirdweb-dev/chains'
 import { useState } from 'react'
 import '../styles/global.css'
 import { Layout } from '../components/layout/Layout'
 
-export default function App({ Component, pageProps }: AppProps<{
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{
   initialSession: Session
 }>) {
   // Create a new supabase browser client on every first render
@@ -17,9 +28,15 @@ export default function App({ Component, pageProps }: AppProps<{
       supabaseClient={supabaseClient}
       initialSession={pageProps.initialSession}
     >
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+      <ThirdwebProvider
+        clientId="2a4f7795555a65af9128f029c3c2b1fc"
+        activeChain={Sepolia}
+        supportedWallets={[metamaskWallet(), localWallet()]}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThirdwebProvider>
     </SessionContextProvider>
   )
 }
