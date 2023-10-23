788a643ea846c2d5152769e4788ab94292a5c6cf
Syaamil Faiq <syaamil1136@gmail.com>
2023-10-23 23:47:55 +0800

    Add ETH price ui

---
+++ frontend/src/components/dashboard/ETHPrice.tsx
@@ -0,0 +1,22 @@
+import { Wallet } from "lucide-react";
+import ETH from "../../../public/icons/ethereum-icon.svg";
+import { Card, CardHeader, CardTitle, CardContent } from "../ui/card";
+
+// TODO: getServerSideProps here
+
+export default function ETHPrice() {
+  return (
+    <>
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between pb-4 space-y-0">
+          <CardTitle className="font-medium">Ethereum Price</CardTitle>
+          <ETH className="w-7 h-7 text-primary" />
+        </CardHeader>
+        <CardContent className="space-y-1">
+          <div className="text-2xl font-bold">$1,679.44</div>
+          <p className="text-success">+0.90%</p>
+        </CardContent>
+      </Card>
+    </>
+  )
+}
