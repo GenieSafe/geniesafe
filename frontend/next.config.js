1161e9c5bc62f4e987ee4a95f3d0c095217cb09a
Syaamil Faiq <syaamil1136@gmail.com>
2023-10-23 23:47:32 +0800

    Readd experiments

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
 module.exports = {
   webpack: (config) => {
     // this will override the experiments
-    // config.experiments = { ...config.experiments, topLevelAwait: true }
+    config.experiments = { ...config.experiments, topLevelAwait: true }
     // this will just update topLevelAwait property of config.experiments
     // config.experiments.topLevelAwait = true
     // Grab the existing rule that handles SVG imports
