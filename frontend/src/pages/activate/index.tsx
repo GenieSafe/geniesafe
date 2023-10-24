9191f6fa2de2dcbb8387825d50c423ccd6d87cb9
alif <alifmazli01@gmail.com>
2023-10-24 23:41:43 +0800

    Prep data to create will on WillContract

--- frontend/src/pages/activate/index.tsx
+++ frontend/src/pages/activate/index.tsx
@@ -12,6 +12,57 @@ import { Input } from '../../components/ui/input'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button } from '../../components/ui/button'
+import { createPagesServerClient } from '@supabase/auth-helpers-nextjs'
+import { GetServerSidePropsContext } from 'next'
+import { Tables } from '../../lib/database.types'
+import { useToast } from '../../components/ui/use-toast'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '../../components/ui/alert-dialog'
+import { useState } from 'react'
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  // Create authenticated Supabase Client
+  const supabase = createPagesServerClient(ctx)
+  // Check if we have a session
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if (!session)
+    return {
+      redirect: {
+        destination: '/auth/login',
+        permanent: false,
+      },
+    }
+
+  // Run queries with RLS on the server
+  const { data, error } = await supabase
+    .from('wills')
+    .select(
+      `
+      id, title, contract_address, deployed_at_block, status,
+      beneficiaries(percentage, metadata:user_id(first_name, last_name, wallet_address)),
+      validators(has_validated, metadata:user_id(first_name, last_name, wallet_address))
+    `
+    )
+    .eq('user_id', session.user.id)
+
+  return {
+    props: {
+      data: data ? data[0] : error,
+    },
+  }
+}
 
 const formSchema = z.object({
   icNumber: z
@@ -21,12 +72,14 @@ const formSchema = z.object({
     }),
 })
 
-export default function Activate() {
+export default function Activate({ data }: { data: any }) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
   })
 
-  const formatICNumber = (icNumber: string) => {
+  const formatIcNumber = (icNumber: string) => {
     // Remove dashes and ensure it's a valid I/C number
     icNumber = icNumber.replace(/-/g, '')
     if (/^\d{12}$/.test(icNumber)) {
@@ -36,15 +89,41 @@ export default function Activate() {
     return icNumber
   }
 
-  const handleICNumberChange = (e: { target: { value: string } }) => {
+  const handleIcNumberChange = (e: { target: { value: string } }) => {
     // Limit the input field to 12 characters
     const inputWithoutDashes = e.target.value.replace(/-/g, '').slice(0, 12)
-    const formattedICNumber = formatICNumber(inputWithoutDashes)
+    const formattedICNumber = formatIcNumber(inputWithoutDashes)
     form.setValue('icNumber', formattedICNumber)
   }
 
-  const onSubmit = (data: any) => {
-    console.log(data)
+  const onSubmit = ({ icNumber }: { icNumber: string }) => {
+    // Close dialog
+    setIsDialogOpen(false)
+    
+    // Check if data is not null
+    if (data === null) return
+
+    // Extract id
+    const _id = data.id
+
+    // Extract wallet addresses from beneficiaries
+    const _beneficiaries = data.beneficiaries.map(
+      (beneficiary: Tables<'beneficiaries'>) =>
+        beneficiary.metadata
+          ? (beneficiary.metadata as { wallet_address: string }).wallet_address
+          : null
+    )
+
+    // Extract percentages
+    const _percentages: number[] = data.beneficiaries.map(
+      (beneficiary: Tables<'beneficiaries'>) => beneficiary.percentage
+    )
+
+    // Log data
+    console.log('icNumber', icNumber)
+    console.log('_id', _id)
+    console.log('_beneficiaries', _beneficiaries)
+    console.log('_percentages', _percentages)
   }
 
   return (
