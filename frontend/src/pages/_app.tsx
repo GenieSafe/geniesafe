2dfbf45d34c7bd3eb632a63025a0aa14a65f5a09
Syaamil Faiq <syaamil1136@gmail.com>
2023-10-24 18:26:53 +0800

    Change global font to Outfit

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,25 +1,40 @@
-
 import type { AppProps } from 'next/app'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { createPagesBrowserClient, Session } from '@supabase/auth-helpers-nextjs'
+import {
+  createPagesBrowserClient,
+  Session,
+} from '@supabase/auth-helpers-nextjs'
 import { useState } from 'react'
 import '../styles/global.css'
 import { Layout } from '../components/layout/Layout'
+import { Outfit} from 'next/font/google'
 
-export default function App({ Component, pageProps }: AppProps<{
+const outfit = Outfit({ subsets: ['latin'] })
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{
   initialSession: Session
 }>) {
   // Create a new supabase browser client on every first render
   const [supabaseClient] = useState(() => createPagesBrowserClient())
 
   return (
-    <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-    </SessionContextProvider>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${outfit.style.fontFamily};
+        }
+      `}</style>
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionContextProvider>
+    </>
   )
 }
