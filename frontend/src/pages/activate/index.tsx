3296942a599186344154996e8991e0c2e0ea7879
alif <alifmazli01@gmail.com>
2023-10-24 19:19:24 +0800

    Add input field with validation

--- frontend/src/pages/activate/index.tsx
+++ frontend/src/pages/activate/index.tsx
@@ -2,6 +2,7 @@ import { z } from 'zod'
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -15,8 +16,9 @@ import { Button } from '../../components/ui/button'
 const formSchema = z.object({
   icNumber: z
     .string()
-    .min(12, 'Must be 12 characters')
-    .max(12, 'Must be 12 characters'),
+    .refine((icNumber) => /^\d{6}-\d{2}-\d{4}$/.test(icNumber), {
+      message: 'Invalid I/C number format',
+    }),
 })
 
 export default function Activate() {
@@ -24,13 +26,30 @@ export default function Activate() {
     resolver: zodResolver(formSchema),
   })
 
-  const onSubmit = (data: z.infer<typeof formSchema>) => {
+  const formatICNumber = (icNumber: string) => {
+    // Remove dashes and ensure it's a valid I/C number
+    icNumber = icNumber.replace(/-/g, '')
+    if (/^\d{12}$/.test(icNumber)) {
+      // Insert dashes in the correct positions as the user types
+      icNumber = icNumber.replace(/(\d{6})(\d{2})(\d{4})/, '$1-$2-$3')
+    }
+    return icNumber
+  }
+
+  const handleICNumberChange = (e: { target: { value: string } }) => {
+    // Limit the input field to 12 characters
+    const inputWithoutDashes = e.target.value.replace(/-/g, '').slice(0, 12)
+    const formattedICNumber = formatICNumber(inputWithoutDashes)
+    form.setValue('icNumber', formattedICNumber)
+  }
+
+  const onSubmit = (data: any) => {
     console.log(data)
   }
 
   return (
     <>
-      <div className="flex items-center justify-between pb-12">
+      <div className="flex items-center justify between pb-12">
         <h1 className="text-4xl font-bold tracking-tight shadow scroll-m-20 lg:text-5xl">
           Activate Will
         </h1>
@@ -42,10 +61,18 @@ export default function Activate() {
             name="icNumber"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>I/C Number</FormLabel>
+                <FormLabel>Deceased's I/C number</FormLabel>
                 <FormControl>
-                  <Input placeholder="E.g., 010101-01-0101" {...field} />
+                  <Input
+                    placeholder=""
+                    {...field}
+                    onChange={handleICNumberChange}
+                    maxLength={12} // Limit the input to 12 characters
+                  />
                 </FormControl>
+                <FormDescription>
+                  Enter digits without dashes (-)
+                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
