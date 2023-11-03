bfc48439c1daec86d7248073c182feb61162e5b9
alif <alifmazli01@gmail.com>
2023-11-04 01:54:50 +0800

    Setup thirdweb

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -32,7 +32,16 @@ module.exports = {
     tests: "./test",
   },
   solidity: {
-    version: "0.8.17",
+    version: "0.8.7",
+    defaultNetwork: "sepolia",
+    networks: {
+      hardhat: {},
+      sepolia: {
+        // url: "https://rpc.ankr.com/eth_sepolia",
+        url: "https://sepolia.rpc.thirdweb.com",
+        accounts: ["0x19882AfC7913B21E2E414F8219eA3bdF3202aB99"],
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
