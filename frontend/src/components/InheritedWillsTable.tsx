de4227b3ef3cc204683438bc01cfbf697a1f5fc4
alif <alifmazli01@gmail.com>
2023-11-04 03:58:13 +0800

    Add activate will logic

--- frontend/src/components/InheritedWillsTable.tsx
+++ frontend/src/components/InheritedWillsTable.tsx
@@ -24,12 +24,17 @@ import { Database } from '../lib/database.types'
 import { useRouter } from 'next/router'
 import { useToast } from './ui/use-toast'
 import { Button } from './ui/button'
-import { useContract, useContractWrite } from '@thirdweb-dev/react'
+import {
+  useConnectionStatus,
+  useContract,
+  useContractWrite,
+} from '@thirdweb-dev/react'
 
 export default function InheritedWillsTable({ data }: { data: any }) {
   const supabase = useSupabaseClient<Database>()
   const router = useRouter()
   const { toast } = useToast()
+  const connectionStatus = useConnectionStatus()
 
   const willContract = process.env.NEXT_PUBLIC_WILL_CONTRACT_ADDRESS
   const { contract } = useContract(willContract)
@@ -40,49 +45,61 @@ export default function InheritedWillsTable({ data }: { data: any }) {
 
   async function handleActivateWill(id: string) {
     // Initialize empty arrays for beneficiaries and percentages
+    let owner = ''
     const _beneficiaries: string[] = []
     const _percentages: number[] = []
 
     // Get will data based on id
-    const will = data.filter((item: any) => item.wills.id === id)[0]
+    const will = data.find((item: any) => item.wills.id === id)
 
-    // Get beneficiaries array from will
-    const beneficiaries = will.wills.beneficiaries
+    if (!will) {
+      console.error('Will not found for id:', id)
+      return
+    }
+
+    // Get owner wallet address from will
+    owner = will.wills.profiles.wallet_address
 
     // Extract beneficiaries and percentages from beneficiaries array
-    // Data is formatted for WillContract
-    for (const beneficiary of beneficiaries) {
+    will.wills.beneficiaries.forEach((beneficiary: any) => {
       _beneficiaries.push(beneficiary.profiles.wallet_address)
       _percentages.push(beneficiary.percentage)
-    }
+    })
 
     // Create will on WillContract
     try {
       const data = await createWill({
-        args: [id, _beneficiaries, _percentages],
+        args: [id, owner, _beneficiaries, _percentages],
       })
+
+      // Show toast and reload page after 2 seconds
+      toast({
+        title: 'Creating will...',
+        description: 'Please wait while we create your will.',
+      })
+
       console.info('Create will successful', data)
+
+      // Update will DB status to ACTIVE
+      const { error: updateWillStatusError } = await supabase
+        .from('wills')
+        .update({ status: 'ACTIVE' })
+        .eq('id', id)
+
+      if (updateWillStatusError) {
+        console.error('Update status failed', updateWillStatusError)
+      } else {
+        toast({
+          title: 'Will activated successfully!',
+          description: 'Validators will be notified via email.',
+        })
+        setTimeout(() => {
+          router.reload()
+        }, 2000)
+      }
     } catch (error) {
       console.error('Create will failed', error)
     }
-
-    // Update will DB status to ACTIVE
-    const { error } = await supabase
-      .from('wills')
-      .update({ status: 'ACTIVE' })
-      .eq('id', id)
-    if (error) console.error('Update status failed', error)
-
-    // Show toast and reload page after 2 seconds
-    window.setTimeout(() => {
-      toast({
-        title: 'Will activated successfully!',
-        description: 'Validators will be notified via email.',
-      })
-      window.setTimeout(() => {
-        router.reload()
-      }, 2000)
-    }, 500)
   }
 
   return (
@@ -94,7 +111,7 @@ export default function InheritedWillsTable({ data }: { data: any }) {
           <TableHead className="text-center">Status</TableHead>
           <TableHead className="text-center">Validation Status</TableHead>
           <TableHead className="text-center">Division</TableHead>
-          <TableHead className="text-right">Action</TableHead>
+          <TableHead className="text-center">Action</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
