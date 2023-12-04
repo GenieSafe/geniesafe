e0043595b5d42a2e95e52b0a2117c101b0874587
Syaamil Faiq <syaamil1136@gmail.com>
2023-12-04 13:45:21 +0800

    Update loading state

--- frontend/src/components/layout/Loading.tsx
+++ frontend/src/components/layout/Loading.tsx
@@ -1,10 +1,12 @@
 import { Loader2 } from 'lucide-react'
+import Geniesafe from '../../../public/icons/geniesafe.svg'
 
 export default function Loading() {
   return (
     <>
-      <div className="flex items-center justify-center w-full h-full">
-        <Loader2 className="w-20 h-20 animate-spin text-primary" />
+      <div className="flex items-center justify-center w-full h-[500px]">
+        <Geniesafe className="h-36 w-36 animate-pulse"/>
+        
       </div>
     </>
   )
