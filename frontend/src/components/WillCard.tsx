ff35729d1685208f532a61a00a1852ec61b2a0df
Syaamil Faiq <syaamil1136@gmail.com>
2024-01-10 19:01:54 +0800

    Refactor WillCard component styling

--- frontend/src/components/WillCard.tsx
+++ frontend/src/components/WillCard.tsx
@@ -31,7 +31,6 @@ export function WillCard({
     <Card className="hover:shadow-[0px_0px_20px_0px_hsl(var(--primary))] transition-shadow duration-500 p-4">
       <CardHeader className="grid grid-cols-2">
         <div className="space-y-2">
-          {/* <CardTitle className="w-fit text-3xl font-semibold tracking-tight scroll-m-20 hover:bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] hover:from-violet-200 hover:via-violet-400 hover:to-violet-800 hover:text-transparent hover:bg-clip-text hover:transition-colors hover:duration-300 hover:ease-in-out"> */}
           <CardTitle className={`w-fit text-3xl font-semibold tracking-tight scroll-m-20 ${will.status == 'INACTIVE' ? "hover:bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] hover:from-violet-200 hover:via-violet-400 hover:to-violet-800 hover:text-transparent hover:bg-clip-text hover:transition-colors hover:duration-300 hover:ease-in-out" : ""}`}>
             <Link
               href={`wills/edit/${will.id}`}
